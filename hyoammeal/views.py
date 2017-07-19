118fffd3cffd80d33697f56f112166bbfae1a756
Hee Ryun, Jin <jins4218@gmail.com>
2017-07-19 18:24:52 +0900

    내일의 급식 메뉴 추가
    Signed-off-by: Hee Ryun, Jin <jins4218@gmail.com>

--- hyoammeal/views.py
+++ hyoammeal/views.py
@@ -3,13 +3,14 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-import json, datetime
+import json, time, datetime
+from datetime import date
 
 # GET ~/keyboard/ 요청에 반응
 def keyboard(request):
     return JsonResponse({
         'type': 'buttons',
-        'buttons': ['조식', '중식', '석식']
+        'buttons': ['조식', '중식', '석식', '내일의 조식', '내일의 중식', '내일의 석식']
     })
 
 # csrf 토큰 에러 방지, POST 요청에 반응
@@ -22,17 +23,33 @@ def message(request):
     dayString = ["월", "화", "수", "목", "금", "토", "일"]
     today = datetime.datetime.today().weekday()
 
-    today_date = datetime.date.today().strftime("%m월 %d일 ")
+    nextdayString = ["화", "수", "목", "금", "토", "일", "월"]
+    tomorrow = datetime.datetime.today().weekday()
 
-    return JsonResponse({
-        'message': {
-            'text': today_date + str(dayString[today]) + '요일 ' + meal + ' 메뉴입니다. \n \n' + str(crawl(request))
-        },
-        'keyboard': {
-            'type': 'buttons',
-            'buttons': ['조식', '중식', '석식']
-        }
-    })
+    today_date = datetime.date.today().strftime("%m월 %d일 ")
+    tomorrow_date = date.fromtimestamp(time.time() + 60*60*24).strftime("%m월 %d일 ")
+
+    if meal == '조식' or meal == '중식' or meal == '석식':
+        return JsonResponse({
+            'message': {
+                'text': today_date + str(dayString[today]) + '요일 ' + meal + ' 메뉴입니다. \n \n' + str(crawl(request))
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['조식', '중식', '석식', '내일의 조식', '내일의 중식', '내일의 석식']
+            }
+     })
+    if meal == '내일의 조식' or meal == '내일의 중식' or meal == '내일의 석식':
+        return JsonResponse({
+            'message': {
+                'text': '['+ meal '] \n' + tomorrow_date + str(nextdayString[tomorrow]) + '요일 급식 메뉴입니다. \n \n' + str(crawl(request))
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['내일의 조식', '내일의 중식', '내일의 석식', '조식', '중식', '석식']
+            }
+     })
+
 
 # message 요청 받을시 크롤링 실시
 def crawl(request):
@@ -45,11 +62,11 @@ def crawl(request):
 
     ScCode = 1
 
-    if meal == '조식':
+    if meal == '조식' or meal == '내일의 조식':
         ScCode = 1
-    if meal == '중식':
+    if meal == '중식' or meal == '내일의 중식':
         ScCode = 2
-    if meal == '석식':
+    if meal == '석식' or meal == '내일의 석식':
         ScCode = 3
 
     # NEIS에서 파싱, 타학교는 schulCode 수정 필요
@@ -70,20 +87,33 @@ def crawl(request):
     today = datetime.datetime.today().weekday()
 
     # 월요일 ~ 일요일 = td[8] ~ td[14]
-    if today == 0:
-        menu = td[8]
-    elif today == 1:
-        menu = td[9]
-    elif today == 2:
-        menu = td[10]
-    elif today == 3:
-        menu = td[11]
-    elif today == 5:
-        menu = td[12]
-    elif today == 6:
-        menu = td[13]
-    elif today == 7:
-        menu = td[14]
+    if meal == '조식' or meal == '중식' or meal == '석식':
+        if today == 0:
+            menu = td[8]
+        elif today == 1:
+            menu = td[9]
+        elif today == 2:
+            menu = td[10]
+        elif today == 3:
+            menu = td[11]
+        elif today == 5:
+            menu = td[12]
+        elif today == 6:
+            menu = td[13]
+
+    elif meal == '내일의 조식' or meal == '내일의 중식' or meal == '내일의 석식':
+        if today == 0:
+            menu = td[9]
+        elif today == 1:
+            menu = td[10]
+        elif today == 2:
+            menu = td[11]
+        elif today == 3:
+            menu = td[12]
+        elif today == 5:
+            menu = td[13]
+        elif today == 6:
+            menu = td[14]
 
     # 파싱 후 불필요한 태그 잔해물 제거
     menu = str(menu).replace(' ', '').replace('*', '').replace('<td', "").replace('<br/>', "\n").replace('class="textC">', '').replace('</td>', '').replace('1.', '').replace('2.', '').replace('3.', '').replace('4.', '').replace('5.', '').replace('6.', '').replace('7.', '').replace('8.', '').replace('9.', '').replace('10.', '').replace('11.', '').replace('12.', '').replace('13.', '').replace('14.', '').replace('15.', '').replace('1', '')
