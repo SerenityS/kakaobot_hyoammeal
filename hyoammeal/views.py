24cf8ba226256bc0df360d16f1590b0a9ddd355b
HeeRyun, Jin <jins4218@gmail.com>
2017-07-24 17:59:56 +0900

    일부 학교에서 급식이 없는데도 제대로 표시가 되지 않던 현상 수정
    Signed-off-by: HeeRyun, Jin <jins4218@gmail.com>

--- hyoammeal/views.py
+++ hyoammeal/views.py
@@ -105,7 +105,7 @@ def crawl(request):
             menu = td[today + 9]
 
     # 파싱 후 불필요한 태그 잔해물 제거
-    menu = str(menu).replace('*', '').replace('<td', "").replace('<br/></td>', "").replace('class="textC last">', '').replace('class="textC">', '').replace('<br/>', '\n').replace('1.', '').replace('2.', '').replace('3.', '').replace('4.', '').replace('5.', '').replace('6.', '').replace('7.', '').replace('8.', '').replace('9.', '').replace('10.', '').replace('11.', '').replace('12.', '').replace('13.', '').replace('14.', '').replace('15.', '').replace('1', '').replace(' ', '')
+    menu = str(menu).replace('*', '').replace('<td', '').replace('<br/>', '').replace('</td>', '').replace('class="textC last">', '').replace('class="textC">', '').replace('<br/>', '\n').replace('1.', '').replace('2.', '').replace('3.', '').replace('4.', '').replace('5.', '').replace('6.', '').replace('7.', '').replace('8.', '').replace('9.', '').replace('10.', '').replace('11.', '').replace('12.', '').replace('13.', '').replace('14.', '').replace('15.', '').replace('1', '').replace(' ', '')
 
     if menu == '':
         menu = '급식이 없습니다.'
